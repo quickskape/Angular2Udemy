d87414cdbd3b8e6e7e8cfae1c736897b76b09933
James Neal <jamesneal@etech.net>
2016-07-01 18:18:33 +0100

    Add material design packages to project

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,14 @@
 import {Component} from "@angular/core"
 import {Course} from "./course"
 import {CoursesService} from "./courses.service"
+import { MD_CARD_DIRECTIVES } from "@angular2-material/card"
+import { MD_BUTTON_DIRECTIVES } from "@angular2-material/button"
 
 @Component({
     selector: "courses",
     templateUrl: "./app/courses/courses.component.html",
-    providers: [CoursesService]
+    providers: [CoursesService],
+    directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES]
 })
 export class CoursesComponent {
     courses: Array<Course>;
