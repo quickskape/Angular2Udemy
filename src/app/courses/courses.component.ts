9f2d99124b6aba4edcb850f989f3c75da0d5ca58
James Neal <jamesneal@etech.net>
2016-07-01 23:28:47 +0100

    Show courses on load

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -19,6 +19,7 @@ export class CoursesComponent {
 
     constructor(private coursesService: CoursesService) {
         this.courses = []
+        this.getCourses()
     }
 
     public getCourses() {
